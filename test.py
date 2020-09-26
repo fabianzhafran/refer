51680020809f4516d86afc4c24ee0e5a76a72a44
fabianzhafran <fabianzhafransyahharahap@gmail.com>
2020-09-26 15:16:41 +0700

    fixed minor bug

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ from refer import REFER
 dataroot = "/projectnb/statnlp/gik/refer/data"
 
 if __name__ == '__main__':
-    refer = REFER(dataset='refcocog',, dataroot splitBy='google')
+    refer = REFER(dataset='refcocog', dataroot splitBy='google')
     ref_ids = refer.getRefIds(split="test")[:]
     print(ref_ids)
