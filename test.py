7b317c4b66fcd58f230e24ac36f96941dea7cc0e
fabianzhafran <fabianzhafransyahharahap@gmail.com>
2020-09-27 21:47:25 +0700

    Chanegd test

--- test.py
+++ test.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     dataroot = "/projectnb/statnlp/gik/refer/data"
     refer = REFER(dataset='refcoco', data_root=dataroot, splitBy='google')
     ref_ids = refer.getRefIds(split="test")[:]
-    print(ref_ids)
+    print(ref_ids[:4])
