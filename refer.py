3efd6b84897860a7921ed0f622530b00ed8caed3
fabianzhafran <fabianzhafransyahharahap@gmail.com>
2020-09-27 21:57:08 +0700

    Update test py see how refer works

--- refer.py
+++ refer.py
@@ -63,6 +63,7 @@ class REFER:
 		self.data = {}
 		self.data['dataset'] = dataset
 		self.data['refs'] = pickle.load(open(ref_file, 'rb'))
+		print(self.data['refs'])
 
 		# load annotations from data/dataset/instances.json
 		instances_file = osp.join(self.DATA_DIR, 'instances.json')
