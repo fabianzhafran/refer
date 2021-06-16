64026e186739ff4657d767e800e77362455bcadb
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2021-06-16 20:32:15 +0700

    debug print dataset

--- train_refcoco.py
+++ train_refcoco.py
@@ -20,6 +20,8 @@ def get_refer_dicts():
     refer = REFER(dataset='refcoco', data_root='./data', splitBy='google')
     ref_ids = refer.getRefIds(split='train')
     refer_train_list = []
+    
+    list_data_image = {}
 
     for ref_id in ref_ids:
         new_ref = {}
@@ -45,7 +47,24 @@ def get_refer_dicts():
         new_ref['file_name'] = "data/images/mscoco/images/train2014/" + refer.loadImgs(ref['image_id'])[0]['file_name']
         new_ref['image_id'] = ref['image_id']
         refer_train_list.append(new_ref)
-    return refer_train_list
+        
+        list_data_image['image_id'] = new_ref
+        
+        
+    final_refer_train_list = []
+    for image_id in list_data_image:
+        final_ref = {}
+        final_ref['image_id'] = image_id
+        final_ref['file_name'] = list_data_image[image_id][0]['file_name']
+        final_ref['height'] = list_data_image[image_id][0]['height']
+        final_ref['width'] = list_data_image[image_id][0]['width']
+        final_ref['annotations'] = []
+        for obj_ref in list_data_image[image_id]:
+            final_ref['annotations'].append(obj_ref['annotations'])
+        
+    print(final_refer_train_list[:5])
+    print(len(final_refer_train_list))
+    return final_refer_train_list
 
 def get_refer_classes():
     refer = REFER(dataset='refcoco', data_root='./data', splitBy='google')
@@ -63,24 +82,24 @@ DatasetCatalog.register("refer_" + d, lambda d=d: get_refer_dicts())
 MetadataCatalog.get("refer_" + d).set(thing_classes=get_refer_classes())
 # refer_metadata = MetadataCatalog.get("refer_train")
 
-NUM_CLASSES = len(get_refer_classes())
-OUTPUT_DIR = "fine_tuned_model"
+# NUM_CLASSES = len(get_refer_classes())
+# OUTPUT_DIR = "fine_tuned_model"
 
-cfg = get_cfg()
-cfg.merge_from_file("/projectnb/statnlp/gik/py-bottom-up-attention/configs/VG-Detection/faster_rcnn_R_101_C4_attr_caffemaxpool.yaml")
-# cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_C4_3x.yaml"))
-cfg.DATASETS.TRAIN = ("refer_train",)
-cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = "https://nlp.cs.unc.edu/models/faster_rcnn_from_caffe_attr.pkl"
-cfg.SOLVER.IMS_PER_BATCH = 2
-cfg.SOLVER.BASE_LR = 0.00025 
-cfg.SOLVER.MAX_ITER = 5000    
-# cfg.SOLVER.STEPS = []        
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 8
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = NUM_CLASSES
+# cfg = get_cfg()
+# cfg.merge_from_file("/projectnb/statnlp/gik/py-bottom-up-attention/configs/VG-Detection/faster_rcnn_R_101_C4_attr_caffemaxpool.yaml")
+# # cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_C4_3x.yaml"))
+# cfg.DATASETS.TRAIN = ("refer_train",)
+# cfg.DATASETS.TEST = ()
+# cfg.DATALOADER.NUM_WORKERS = 2
+# cfg.MODEL.WEIGHTS = "https://nlp.cs.unc.edu/models/faster_rcnn_from_caffe_attr.pkl"
+# cfg.SOLVER.IMS_PER_BATCH = 2
+# cfg.SOLVER.BASE_LR = 0.00025 
+# cfg.SOLVER.MAX_ITER = 5000    
+# # cfg.SOLVER.STEPS = []        
+# cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 8
+# cfg.MODEL.ROI_HEADS.NUM_CLASSES = NUM_CLASSES
 
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg) 
-trainer.resume_or_load(resume=False)
-trainer.train()
+# os.makedirs(OUTPUT_DIR, exist_ok=True)
+# trainer = DefaultTrainer(cfg) 
+# trainer.resume_or_load(resume=False)
+# trainer.train()
