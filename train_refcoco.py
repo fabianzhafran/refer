303151516583a24845733fa4525569b3b3dd8760
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2021-06-16 20:40:32 +0700

    fix syntax

--- train_refcoco.py
+++ train_refcoco.py
@@ -49,7 +49,7 @@ def get_refer_dicts():
         refer_train_list.append(new_ref)
         
         if ref['image_id'] in list_data_image:
-            list_data_image[ref['image_id']].push(new_ref)
+            list_data_image[ref['image_id']].append(new_ref)
         else:
             list_data_image[ref['image_id']] = new_ref
         
@@ -63,7 +63,7 @@ def get_refer_dicts():
         final_ref['width'] = list_data_image[image_id][0]['width']
         final_ref['annotations'] = []
         for obj_ref in list_data_image[image_id]:
-            final_ref['annotations'].append(obj_ref['annotations'])
+            final_ref['annotations'] += obj_ref['annotations']
         
     print(final_refer_train_list[:5])
     print(len(final_refer_train_list))
